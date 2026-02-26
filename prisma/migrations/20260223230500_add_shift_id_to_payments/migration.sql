ddef3b68246600ed1ceb1c1a65f53cbfb31320d6
truthysys <truthysys@gmail.com>
2026-02-26 12:43:26 +0000

    folder wron

--- prisma/migrations/20260223230500_add_shift_id_to_payments/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "payments" ADD COLUMN "shift_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_shift_id_fkey" FOREIGN KEY ("shift_id") REFERENCES "shifts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
